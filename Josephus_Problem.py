b3eabe18ec260f3d7d039e223cd4846d5a2da800
Logeshkumar12 <114285406+Logeshkumar12@users.noreply.github.com>
2024-08-06 10:56:40 +0530

    Update Josephus_Problem.py

--- Josephus_Problem.py
+++ Josephus_Problem.py
@@ -3,23 +3,17 @@
 #In each step, a certain number of people are skipped and the next person is executed. The elimination proceeds around the circle 
 #(which is becoming smaller and smaller as the executed people are removed), until only the last person remains, who is given freedom. 
 
-def Josh(person, k, index):
+def Josh(person, k, index) -> int:
     if len(person) == 1:
-        print(person[0])
-        return
+        return person[0]
     
     index = ((index+k)%len(person))
-    print("Count:" ,len(person))
-    print("index :",index)
     person.pop(index)
-    print(person)
-    
     Josh(person,k,index)
 
 
 n = int(input("No of Person: "))
 k = int(input("Skip value: "))
-k-=1
 index = 0
 person=list(range(1,n+1))
-Josh(person,k,index) 
+print("Winner: ",Josh(person,k-1,index) 
